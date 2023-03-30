2280611da5e297e215949a2ef517bed8e3107172
Matt <madm4tty@users.noreply.github.com>
2023-03-30 18:25:19 +0000

    Working street names column updates on pan and zoom

--- main.py
+++ main.py
@@ -4,9 +4,11 @@ import os
 app = Flask(__name__)
 google_maps_api_key = os.getenv("GOOGLE_MAPS_API_KEY")
 
+
 @app.route('/')
 def index():
-    return render_template('index.html', google_maps_api_key=google_maps_api_key)
+  return render_template('index.html', google_maps_api_key=google_maps_api_key)
+
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+  app.run(host='0.0.0.0', port=8080)
